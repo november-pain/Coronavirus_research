ae6aa6593439ee43839d7d7feb135b6dc3aece7f
november-pain <max.chikita@gmail.com>
2020-04-02 20:46:43 +0300

    unzip amendment

--- examples/api_example.py
+++ examples/api_example.py
@@ -1,9 +1,6 @@
 from kaggle.api.kaggle_api_extended import KaggleApi
-import zip_processor as zp
 
 api = KaggleApi()
 api.authenticate()
 
-api.dataset_download_files('imdevskp/corona-virus-report')
-
-zp.ZipProcessor('corona-virus-report.zip').unzip_files()
+api.dataset_download_files('imdevskp/corona-virus-report', unzip=True)
