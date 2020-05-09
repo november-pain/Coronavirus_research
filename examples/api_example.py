4594f22a8f112ff3032d071adec0d8a55e91fd34
november-pain <max.chikita@gmail.com>
2020-05-09 16:44:03 +0300

    data update

--- examples/api_example.py
+++ examples/api_example.py
@@ -4,3 +4,5 @@ api = KaggleApi()
 api.authenticate()
 
 api.dataset_download_files('imdevskp/corona-virus-report', unzip=True)
+
+api.dataset_download_files('sudalairajkumar/covid19-in-usa', unzip=True)
