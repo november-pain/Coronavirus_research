713aa4f9d584087b6403d72cdae7eb12bbd96d96
november-pain <max.chikita@gmail.com>
2020-05-24 04:36:37 +0300

    code cleaning, docs added

--- app/web_app.py
+++ app/web_app.py
@@ -3,15 +3,27 @@ import us
 
 app = Flask(__name__)
 
-with open("/app/calculations", "r") as f:
-    coeff_dict = {}
-    for row in f.readlines():
-        string = row.strip().split(",")
-        coeff_dict[string[0]] = (string[1], string[2])
+
+def read_results(path):
+    """
+    reads file with results of calculations
+    :param path: str
+    :return: dict
+    """
+    with open(path, "r") as f:
+        coefficient_dict = {}
+        for row in f.readlines():
+            string = row.strip().split(",")
+            coefficient_dict[string[0]] = (string[1], string[2])
+    return coefficient_dict
 
 
 @app.route("/", methods=['POST', 'GET'])
 def enter():
+    """
+    search form for app
+    rendering templates
+    """
     input_form = request.form.get("input")
     if input_form is None or input_form == "":
         input_form = "NY"
@@ -19,19 +31,25 @@ def enter():
     if isinstance(state_name, us.states.State):
         state_name = state_name.abbr.upper()
         filename = "{}.png".format(state_name)
-        string1 = coeff_dict[state_name][0]
-        string2 = coeff_dict[state_name][1]
+        string1 = res_dict[state_name][0]
+        string2 = res_dict[state_name][1]
         return render_template("us_state.html",
                                path=f"static/images/{filename}",
                                postot=string1, negtot=string2,
-                               usa_postot=coeff_dict["USA"][0],
-                               usa_negtot=coeff_dict["USA"][1])
+                               usa_postot=res_dict["USA"][0],
+                               usa_negtot=res_dict["USA"][1])
 
 
 @app.route("/", methods=['POST'])
 def state():
+    """
+    renders base template
+    to avoid empty promt error on start
+    """
     return render_template("base.html")
 
 
 if __name__ == '__main__':
+    file_path = "/app/calculations"
+    res_dict = read_results(file_path)
     app.run(debug=True)
